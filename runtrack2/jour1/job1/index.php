a5e2889f1a80bda8bc5eb25dd3832fe1f8013000
Meriamgdi <meriam.goudadi@laplateforme.io>
2025-01-20 16:06:09 +0100

    ajout d'une fonction pour l'affichage de la valeur job3 jour1

--- runtrack2/jour1/job1/index.php
+++ runtrack2/jour1/job1/index.php
@@ -1,8 +1,9 @@
 <?php
-$str = "La Plateforme";
-$str2 = "Vive";
-$str3 = "!";
-echo "$str2 $str $str3";
+$str = 'La Plateforme';
+$str2 = 'Vive';
+$str3 = '!';
+echo "$str2 $str $str3 <br>";
+echo "$str2" . ' ' . "$str" . ' ' . "$str3<br>"; /* autre manière de l'afficher mais sans espace */
 ?>
 
 <?php
@@ -13,5 +14,17 @@ echo "$val" + "$val1";
 
 <?php
 $mybool = true;
-echo "la valeur est égal à true";
+echo "<br>la valeur est égale à" . ' ' . ($mybool ? 'true': 'false');
+?>
+
+<!-- exercice bonus par moi-même pour moi-même -->
+<?php
+$nom = 'Goudadi';
+$prenom = 'Mériam';
+$note1 = 15;
+$note2 = 19;
+$moyenne = ($note1 + $note2) / 2;
+
+echo '<br>Mlle ' . "$nom". ' ' . "$prenom" . ' ' . 'a eu une moyenne de ' . (($note1 + $note2) / 2) . ' sur 20 !<br>';
+echo "Mlle $nom $prenom a eu $moyenne de moyenne."
 ?>
