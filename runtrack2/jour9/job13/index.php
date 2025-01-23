dea0a3546a8eeb9ec7932842eb9e8ff5f82e44d4
Meriamgdi <meriam.goudadi@laplateforme.io>
2025-01-23 16:04:27 +0100

    ajout job13 jour9 : afficher le nom de l'étage et le nom de la salle correspondante avec php dans un tableau html en récupérant les données dans la bdd/sql

--- runtrack2/jour9/job13/index.php
+++ runtrack2/jour9/job13/index.php
@@ -1,1 +1,45 @@
-test
+<?php
+    $hostname = "localhost";
+    $dbname = 'jour08';
+    $username = 'root';
+    $password = '';
+            
+    //On établit la connexion
+    $conn = new mysqli($hostname, $username, $password, $dbname);
+    $resultat = $conn->query("SELECT étages.nom as nom_étage, salles.nom as nom_salle
+        FROM étages JOIN salles WHERE salles.ID_étage = étages.ID;");
+    $salles = $resultat->fetch_all(MYSQLI_ASSOC);
+
+    $conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Nom des salles et Nom de leur étage</h1>
+
+        <table border='1'>
+            <thead>
+                <tr>
+                    <th>Etage</th>
+                    <th>Salle</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                    foreach ($salles as $champs) {
+                        echo ("<tr>
+                            <td>" . $champs['nom_étage'] . "</td>
+                            <td>" . $champs['nom_salle'] . "</td>
+                        </tr>");
+                    }
+                ?>
+            </tbody>
+        </table>
+    </body>
+</html>
