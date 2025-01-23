f1eb5cd70759fc294c9b6f014417502f52503884
Meriamgdi <meriam.goudadi@laplateforme.io>
2025-01-23 14:50:33 +0100

    ajout job9 jour9 afficher la capacité des salles par ordre décroissant en récupérant la bdd/sql de la table salles avec php

---
+++ runtrack2/jour9/job9/index.php
@@ -0,0 +1,49 @@
+<?php
+    $hostname = "localhost";
+    $dbname = 'jour08';
+    $username = 'root';
+    $password = '';
+            
+    //On établit la connexion
+    $conn = new mysqli($hostname, $username, $password, $dbname);
+    $resultat = $conn->query("SELECT * FROM salles ORDER BY capacité DESC");
+    $salles = $resultat->fetch_all(MYSQLI_ASSOC);
+
+    $conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Capacité des salles par ordre décroissant</h1>
+
+        <table border='1'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Etage</th>
+                    <th>Capacité</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                    foreach ($salles as $champs) {
+                        echo ("<tr>
+                            <td>" . $champs['ID'] . "</td>
+                            <td>" . $champs['nom'] . "</td>
+                            <td>" . $champs['ID_étage'] . "</td>
+                            <td>" . $champs['capacité'] . "</td>
+                        </tr>");
+                    }
+                ?>
+
+            </tbody>
+        </table>
+    </body>
+</html>
